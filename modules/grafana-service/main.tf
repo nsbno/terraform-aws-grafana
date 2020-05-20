c4ebf772e605d9016dc16f38466d777bb32b4215
Colin Coleman <colin.coleman@kontrollsmart.com>
2020-05-20 17:49:11 +0200

    test use of new fargate module

--- modules/grafana-service/main.tf
+++ modules/grafana-service/main.tf
@@ -38,7 +38,7 @@ module "grafana_rds" {
 # Grafana ECS Fargate Service
 # ----------------------------------------
 module "grafana-service" {
-  source  = "github.com/nsbno/terraform-aws-ecs-fargate?ref=e6bf17f"
+  source  = "github.com/nsbno/terraform-aws-ecs-fargate?ref=0682dae"
   name_prefix             = var.name_prefix
   vpc_id                  = var.vpc_id
   cluster_id              = var.cluster_id
