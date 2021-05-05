ffe588d5b69e17184ff4972ca0cce7ad12f38029
Nicolas Harlem Eide <nicolas@harlemeide.net>
2021-05-05 13:20:30 +0200

    Add ALB and Target Group ARN suffixes as outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,14 @@ output "url" {
   value = module.grafana-service.url
 }
 
+output "alb_arn_suffix" {
+  value = module.lb.arn_suffix
+}
+
+output "target_group_name" {
+  value = module.grafana-service.target_group_name
+}
+
+output "target_group_arn_suffix" {
+  value = module.grafana-service.target_group_arn_suffix
+}
